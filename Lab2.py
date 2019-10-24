dae9be1bef3b0a72f90b9d65c30a25e8acfef6c0
antonmartinus73 <antonmartinus72@gmail.com>
2019-10-24 23:53:28 +0700

    commit pertama

---
+++ Lab2.py
@@ -0,0 +1,11 @@
+a = input("masukan nilai a:")
+b = input("masukan nilai b:")
+print("variable a=", a)
+print("variable b=", b)
+print("hasil penggabungan {}%{}=%d".format(a,b)%(a+b))
+
+a=int(a)
+b=int(b)
+print("Hasil penjumlahan{a}+{b}=%s".format(a,b)%(a+b))
+print("Hasil pembagian{a}/{b}=%s".format(a,b)%(a+b))
+
